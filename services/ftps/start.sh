7213bc94be0c068a5fe566d688a8126ad5cb63bb
Cyberduck79 <flavien.henrion@protonmail.com>
2020-09-18 14:59:42 +0200

    phpmyadmin

--- services/ftps/start.sh
+++ services/ftps/start.sh
@@ -2,4 +2,4 @@ mkdir -p /ftps/$FTP_USER
 adduser -h /ftps/$FTP_USER -D $FTP_USER
 echo "$FTP_USER:$FTP_PASS" | chpasswd
 chown $FTP_USER:$FTP_USER /ftps/$FTP_USER
-/usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21010 -opasv_address=IP10 /etc/vsftpd/vsftpd.conf
+/usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21010 -opasv_address=172.17.0.10 /etc/vsftpd/vsftpd.conf
--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ Yellow="\n\e[33;1m"
 Light_yellow="\e[33m"
 Default="\e[0m\n"
 ### SERVICES
-services="ftps nginx mariadb"
+services="ftps nginx mariadb wordpress phpmyadmin"
 
 minikube_install() {
     printf "${Yellow}🦆 - QUACK QUACK ! (Let's install minikube !)${Default}"
@@ -15,7 +15,7 @@ minikube_install() {
     printf "${Light_yellow}🦆 - quack quack... (I install...)${Default}"
     curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && \
     chmod +x minikube && sudo install minikube /usr/local/bin/ && rm minikube &&
-    printf "${Green}Minikube installed${Default}" || printf "${Red}Minikube installation failed${Default}"
+    printf "${Green}Minikube installed${Default}" || { printf "${Red}Minikube installation failed${Default}" && exit 1; }
 }
 
 minikube_start() {
@@ -26,7 +26,7 @@ minikube_start() {
     minikube delete > /dev/null
     rm -rf ~/.minikube
     minikube start --driver=docker > /dev/null && \
-    printf "${Green}Minikube started${Default}" || printf "${Red}Minikube start failed${Default}"
+    printf "${Green}Minikube started${Default}" || { printf "${Red}Minikube start failed${Default}" && exit 1; }
 }
 
 minikube_configure() {
@@ -43,14 +43,17 @@ minikube_configure() {
     kubectl create secret generic wordpress-secret \
     --from-literal=db_name='wordpress' --from-literal=db_user='flavien' \
     --from-literal=db_host='mariadb-svc' --from-literal=db_password='T0urn3v1s' > /dev/null && \
-    printf "${Green}Configuration generated${Default}" || printf "${Red}Configuration failed${Default}"
+    kubectl create secret generic phpmyadmin-secret \
+    --from-literal=pma_host='mariadb-svc' --from-literal=pma_port='3306' \
+    --from-literal=pma_user='flavien' > /dev/null && \
+    printf "${Green}Configuration generated${Default}" || { printf "${Red}Configuration failed${Default}" && exit 1; }
     eval $(minikube docker-env)
 
     printf "${Light_yellow}🦆 - quack quack... (I activate addons...)${Default}"
     minikube addons enable metrics-server > /dev/null && \
     minikube addons enable dashboard > /dev/null && \
     minikube addons enable metallb > /dev/null && \
-    printf "${Green}Addons activated${Default}" || printf "${Red}addons activation failed${Default}"
+    printf "${Green}Addons activated${Default}" || { printf "${Red}addons activation failed${Default}" && exit 1; }
     SERVER_IP=$(minikube ip | grep -oE "\b([0-9]{1,3}\.){3}\b")
     sed -i.bak "s/IP/"$SERVER_IP"/g" config_map.yml
     sed -i.bak "s/http:\/\/IP/http:\/\/"$SERVER_IP"/g" services/nginx/index.html
@@ -75,7 +78,7 @@ build_images() {
     do
         printf "${Light_yellow}🦆 - quack quack... (I build $service...)${Default}"
         docker build services/$service -t $service-img > /dev/null && \
-        printf "${Green}${service} image builded${Default}" || printf "${Red}${service} image building failed${Default}"
+        printf "${Green}${service} image builded${Default}" || { printf "${Red}${service} image building failed${Default}" && exit 1; }
     done
 }
 
@@ -84,41 +87,40 @@ apply_configuration() {
 
     printf "${Light_yellow}🦆 - quack quack... (I apply metallb configuration...)${Default}"
     kubectl apply -f config_map.yml > /dev/null && \
-    printf "${Green}Load Balancer configuration applied${Default}" || printf "${Red}Load Balancer configuration failed${Default}"
+    printf "${Green}Load Balancer configuration applied${Default}" || { printf "${Red}Load Balancer configuration failed${Default}" && exit 1; }
 
     for service in $services
     do
         printf "${Light_yellow}🦆 - quack quack... (I apply $service configuration...)${Default}"
         kubectl apply -f services/${service}/deployment.yml > /dev/null && \
-        printf "${Green}${service} deployment configuration applied${Default}" || printf "${Red}${service} deployment failed${Default}"
+        printf "${Green}${service} deployment configuration applied${Default}" || { printf "${Red}${service} deployment failed${Default}" && exit 1; }
         kubectl apply -f services/${service}/service.yml > /dev/null && \
-        printf "${Green}${service} service configuration applied${Default}" || printf "${Red}${service} service failed${Default}"
+        printf "${Green}${service} service configuration applied${Default}" || { printf "${Red}${service} service failed${Default}" && exit 1; }
     done
-    sed -i.bak "s/"$SERVER_IP"/IP/g" config_map.yml
-    sed -i.bak "s/http:\/\/"$SERVER_IP"/http:\/\/IP/g" services/nginx/index.html
+    rm config_map.yml && mv config_map.yml.bak config_map.yml
+    rm services/nginx/index.html && mv services/nginx/index.html.bak services/nginx/index.html
     #sed -i.bak "s/http:\/\/"$SERVER_IP"/http:\/\/IP/g" services/mariadb/wordpress.sql
-    sed -i.bak "s/address="$SERVER_IP"/address=IP/g" services/ftps/start.sh
+    rm services/ftps/start.sh && mv services/ftps/start.sh.bak services/ftps/start.sh
 }
 
 #################################################
 
 report() {
     printf "${Light_yellow}🦆 - quack quack... (Producing reports with my tiny wings...)${Default}"
-    minikube dashboard > /dev/null &
-    sleep 6
+    sleep 10
     printf "${Yellow}Load balancer${Default}"
     kubectl describe configmap config -n metallb-system
     printf "${Yellow}Pods${Default}"
     kubectl get pods
     printf "${Yellow}Services${Default}"
     kubectl get services
+    minikube dashboard > /dev/null &
 }
 
 #################################################
 
-# -> add a arg for deleting minikube or not [TO TEST]
 # -> mettre ficher de conf image dans dossier
-# -> tunnel au lieu de node port ?
+clear
 printf "${Yellow}🦆🦆🦆 FT_DUCKS 🦆🦆🦆${Default}"
 setup_minikube
 build_images
